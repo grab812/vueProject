b52b6f15778a520e4e92e109ac51fe08748d1cea
rameee <2velyram@gmail.com>
2022-11-22 17:19:15 +0900

    vue작업 초기

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,97 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <modalView v-if="modalState" @close-modal="modalState = false">
+
+  </modalView>
+
+
+  <div class="menu">
+    <a v-for="menuTab in manuTabs" :key="menuTab">{{menuTab}}</a>
+  </div>
+
+
+  <!-- <div v-for="(item,i) in rooms" :key="i">
+    <img :src="require(`@/assets/room${i}.jpg`)" alt="" class="room-img">
+    <h4 @click="modalState = !modalState">{{item.product}}</h4>
+    <p>가격 : {{item.price}}만원</p>
+    <button @click="checkNum[i]++">허위매물신고</button><span>신고수: {{checkNum[i]}}</span>
+  </div> -->
+
+  <div v-for="(item,i) in onerooms" :key="i" @click="info($event, i)">
+    <img :src="onerooms[i].image" alt="" class="room-img">
+    <h4 v-html="this.onerooms[i].price"></h4>
+    <p>가격 : {{onerooms[i].price}}만원</p>
+    <!-- <button @click="checkNum[i]++">허위매물신고</button><span>신고수: {{checkNum[i]}}</span> -->
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import 작명 from "경로";
+// import { 변수 } from "./data/post.js";
+
+// import oneroomDate from "./data/oneroomDate.js"; 확장자 생략가능
+import oneroomDate from "./data/oneroomDate";
+import modalView from "./components/modalView";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    modalView,
+  },
+  // 데이터바인딩
+  data() {
+    return {
+      selected: '',
+      modalState: false,
+      manuTabs: ["Home", "Product", "About"],
+      rooms: [
+        {product: '마포구원룸', price:50},
+        {product: '노원구원룸', price:90},
+        {product: '장안구원룸', price:70},
+      ],
+      checkNum: [0,2,0],
+      onerooms: oneroomDate,
+    }
+  },
+  // 함수 function
+  methods: {
+    increase(){
+      this.checkNum++;
+    },
+    info(item,i){
+      // tit.innerText = this.onerooms[i].title;
+      this.modalState = !this.modalState;
+      console.log(this.modalState,this.onerooms[i].price, i);
+    }
   }
 }
 </script>
 
 <style>
+body {
+  margin: 0; 
+}
+div {
+  box-sizing: border-box;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+.menu {
+  background: salmon;
+  border-radius: 10px;
+  padding: 10px;
+}
+.menu a {
+  color: white;
+  padding: 10px;
+}
+.room-img {
+  width: 100%;
+  margin-top: 40px;
 }
 </style>
---
+++ src/components/modalView.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="black-bg">
+      <div class="white-bg" >
+          <!-- <h4 >{{onerooms[i].title}}</h4>
+          <p>{{onerooms[i].price}}</p> -->
+          <button class="modalClose" @click="$emit('close-modal')">닫기</button>
+      </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'modalView',
+  // 데이터바인딩
+  data() {
+    return {
+      modalState: false,
+    }
+  },
+};
+</script>
+<style>
+div {
+  box-sizing: border-box;
+}
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, .5);
+  position: fixed;
+  padding: 20px;
+}
+.white-bg {
+  position: relative;
+  width: 100%;
+  background: rgba(255, 255, 255, 1);
+  border-radius: 8px;
+  padding: 20px;
+}
+.modalClose {
+  position: absolute;
+  top: 3px;
+  right: 3px;
+  width: 18px;
+  height: 18px;
+  text-indent: -9999px;
+  font-size: 0;
+  line-height: 0;
+  padding: 5px;
+  box-sizing: content-box;
+  border: none;
+  background-color: transparent;
+}
+.modalClose:before, .modalClose:after {
+  position: absolute;
+  top: 0;
+  left: 13px;
+  content: ' ';
+  height: 30px;
+  width: 2px;
+  background-color: #000000;
+}
+.modalClose:before {
+  transform: rotate(45deg);
+}
+.modalClose:after {
+  transform: rotate(-45deg);
+}
+</style>
